70fc5c8f11e85c8a620f45939de67d78f9470b41
Lukasz Grabski <lukasz.g@yggdrasilgaming.com>
2017-10-28 15:22:40 +0200

    Parsing first file.

---
+++ backend/src/test/java/com/vikings/hackaton/demo/parser/AddressesDictionaryParserTest.java
@@ -0,0 +1,21 @@
+package com.vikings.hackaton.demo.parser;
+
+
+import org.junit.Test;
+
+import java.io.IOException;
+
+/**
+ * @author lukaszgrabski
+ */
+public class AddressesDictionaryParserTest {
+
+  @Test
+  public void shouldLoadAllAdressesFromCSVFile() throws IOException {
+    // given
+    AddressesDictionaryReader dictionaryReader = new AddressesDictionaryReader(getClass().getClassLoader().getResourceAsStream("addresses.csv"));
+
+    // when
+    dictionaryReader.parseAddresses();
+  }
+}
