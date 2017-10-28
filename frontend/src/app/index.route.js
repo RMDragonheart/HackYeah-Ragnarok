97c65a44a23be542575b097d5570fe53f341c330
Pawel Gawlas <pagawlas@gmail.com>
2017-10-28 13:53:24 +0200

    FRONTEND: add login page

--- frontend/src/app/index.route.js
+++ frontend/src/app/index.route.js
@@ -7,6 +7,7 @@
 	function routeConfig($routeProvider) {
 		$routeProvider
 			.when('/', { templateUrl: 'app/pages/main/main.html' })
+			.when('/login', { templateUrl: 'app/pages/login/login.html' })
 			.otherwise({ redirectTo: '/' });
 	}
 
